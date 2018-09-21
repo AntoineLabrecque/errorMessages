353e7c84cce7c18877274c553e3995a6b338d949
Antoine Labrecque <ajql825@hotmail.com>
2018-09-21 17:00:49 -0600

    Tried to get the modified date of files.

--- make tests.bat
+++ make tests.bat
@@ -6,3 +6,8 @@ g++ -Wall -Wextra -pedantic -c Node.cpp
 g++ -Wall -Wextra -pedantic -c list.cpp
 g++ -Wall -Wextra -pedantic -c tests.cpp
 g++ -Wall -Wextra -pedantic -o tests.exe tests.o list.o Node.o item.o protectedChar.o
+
+for %%source in ( forfiles /m *.cpp /c "echo @fname" ) do
+(
+    if ( dir /b %%source.* /o-d
+)
